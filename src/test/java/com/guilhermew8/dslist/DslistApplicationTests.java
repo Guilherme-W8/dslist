505d23abc3a48ec762963d05d7552dabd3a6828b
Guilherme Correia <gtavares739@gmail.com>
2023-11-14 14:50:20 -0300

    Projeto criado

---
+++ src/test/java/com/guilhermew8/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermew8.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
